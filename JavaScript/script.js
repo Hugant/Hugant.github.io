cbe58b135fc642264a031d95d6dda31b8aed1918
Hugant <HugantMD@yandex.ru>
2016-11-24 00:27:58 +0700

    Fix css files, added devise.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,65 +1,72 @@
-// ------------- VARIABLES ------------- //
-var ticking = false;
-var isFirefox = (/Firefox/i.test(navigator.userAgent));
-var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
-var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive) 
-var slideDurationSetting = 600; //Amount of time for which slide is "locked"
-var currentSlideNumber = 0;
-var totalSlideNumber = $(".background").length;
+console.log(device.mobile());
+console.log(device.tablet());
+if(!device.mobile() && !device.tablet()) {
 
-// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
-function parallaxScroll(evt) {
-  if (isFirefox) {
-    //Set delta for Firefox
-    delta = evt.detail * (-120);
-  } else if (isIe) {
-    //Set delta for IE
-    delta = -evt.deltaY;
-  } else {
-    //Set delta for all other browsers
-    delta = evt.wheelDelta;
-  }
+	// ------------- VARIABLES ------------- //
+	var ticking = false;
+	var isFirefox = (/Firefox/i.test(navigator.userAgent));
+	var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
+	var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive) 
+	var slideDurationSetting = 600; //Amount of time for which slide is "locked"
+	var currentSlideNumber = 0;
+	var totalSlideNumber = $(".background").length;
 
-  if (ticking != true) {
-    if (delta <= -scrollSensitivitySetting) {
-      //Down scroll
-      ticking = true;
-      if (currentSlideNumber !== totalSlideNumber - 1) {
-        currentSlideNumber++;
-        nextItem();
-      }
-      slideDurationTimeout(slideDurationSetting);
-    }
-    if (delta >= scrollSensitivitySetting) {
-      //Up scroll
-      ticking = true;
-      if (currentSlideNumber !== 0) {
-        currentSlideNumber--;
-      }
-      previousItem();
-      slideDurationTimeout(slideDurationSetting);
-    }
-  }
-}
+	// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
+	function parallaxScroll(evt) {
+	  if (isFirefox) {
+		//Set delta for Firefox
+		delta = evt.detail * (-120);
+	  } else if (isIe) {
+		//Set delta for IE
+		delta = -evt.deltaY;
+	  } else {
+		//Set delta for all other browsers
+		delta = evt.wheelDelta;
+	  }
 
-// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
-function slideDurationTimeout(slideDuration) {
-  setTimeout(function() {
-    ticking = false;
-  }, slideDuration);
-}
+	  if (ticking != true) {
+		if (delta <= -scrollSensitivitySetting) {
+		  //Down scroll
+		  ticking = true;
+		  if (currentSlideNumber !== totalSlideNumber - 1) {
+			currentSlideNumber++;
+			nextItem();
+		  }
+		  slideDurationTimeout(slideDurationSetting);
+		}
+		if (delta >= scrollSensitivitySetting) {
+		  //Up scroll
+		  ticking = true;
+		  if (currentSlideNumber !== 0) {
+			currentSlideNumber--;
+		  }
+		  previousItem();
+		  slideDurationTimeout(slideDurationSetting);
+		}
+	  }
+	}
 
-// ------------- ADD EVENT LISTENER ------------- //
-var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
-window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
+	// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
+	function slideDurationTimeout(slideDuration) {
+	  setTimeout(function() {
+		ticking = false;
+	  }, slideDuration);
+	}
 
-// ------------- SLIDE MOTION ------------- //
-function nextItem() {
-  var $previousSlide = $(".background").eq(currentSlideNumber - 1);
-  $previousSlide.removeClass("up-scroll").addClass("down-scroll");
-}
+	// ------------- ADD EVENT LISTENER ------------- //
+	var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+	window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
 
-function previousItem() {
-  var $currentSlide = $(".background").eq(currentSlideNumber);
-  $currentSlide.removeClass("down-scroll").addClass("up-scroll");
+	// ------------- SLIDE MOTION ------------- //
+	function nextItem() {
+	  var $previousSlide = $(".background").eq(currentSlideNumber - 1);
+	  $previousSlide.removeClass("up-scroll").addClass("down-scroll");
+	}
+
+	function previousItem() {
+	  var $currentSlide = $(".background").eq(currentSlideNumber);
+	  $currentSlide.removeClass("down-scroll").addClass("up-scroll");
+	}
+} else {
+	
 }
