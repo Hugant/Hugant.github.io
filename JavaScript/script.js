6d7af5f8180484434ae1072a943166e898023665
Hugant <HugantMD@yandex.ru>
2017-06-10 02:45:25 +0700

    Modified blur alghoritm

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,11 @@
 
-window.onload = function() {
-
+window.onload = function() {//??????
     var timer = setTimeout(function() {
-        document.getElementById("lower_wallpaper").style.backgroundImage = "url(Images/back.jpg)";
         var opacity = 1;
         var interval = setInterval(function() {
             opacity -= 0.023;
             document.getElementById("upper_wallpaper").style.opacity = opacity + "";
-            if (opacity == 0) {
+            if (opacity <= 0) {
                 clearInterval(interval);
             }
         }, 100);
