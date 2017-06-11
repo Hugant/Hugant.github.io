5da902db03d8f41caa3ae9dc81c9549d62bc8cf8
Hugant <HugantMD@yandex.ru>
2017-06-11 13:00:44 +0700

    Modified background of header

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,12 +3,12 @@ window.onload = function() {//??????
     var timer = setTimeout(function() {
         var opacity = 1;
         var interval = setInterval(function() {
-            opacity -= 0.023;
+            opacity -= 0.001;
             document.getElementById("upper_wallpaper").style.opacity = opacity + "";
             if (opacity <= 0) {
                 clearInterval(interval);
             }
-        }, 100);
+        }, 10);
 
     }, 1000);
 
